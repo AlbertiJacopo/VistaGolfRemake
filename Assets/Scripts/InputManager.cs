5181c1c5f622266b3a99c4ff6c6f95061e6298fb
Tasogare_no_Jack <jacopoalberti363@gmail.com>
2023-10-10 12:53:41 +0200

    setup input manager from UML diagram

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private float m_DeadZoneSwingRadius;
+    [SerializeField] private float m_DeadZoneBallRadius;
+    //private DirectionType m_DirectionType;
+    private GameObject m_Ball;
+    private Vector2 m_TouchStartPosition;
+    private Vector2 m_TouchEndPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetStartEndTouches()
+    {
+
+    }
+
+    public bool CheckDeadzoneBall()
+    {
+        return true;
+    }
+}
