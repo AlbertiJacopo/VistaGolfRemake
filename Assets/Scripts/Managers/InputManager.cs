d2255ab27809773d599cd1339fd98b1686ed929f
Alien089 <gabri.moratelli@gmail.com>
2023-10-10 17:27:21 +0200

    updated inputManager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
@@ -7,28 +8,48 @@ public class InputManager : MonoBehaviour
     [SerializeField] private float m_DeadZoneSwingRadius;
     [SerializeField] private float m_DeadZoneBallRadius;
     private GameObject m_Ball;
-    private Vector2 m_TouchStartPosition;
-    private Vector2 m_TouchEndPosition;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    private Vector3 m_TouchStartPosition;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount == 0) return;
+
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            GetStartTouch(touch);
+
+            if (CheckInDeadzoneBall())
+                GameManager.instance.EventManager.TriggerEvent(Constants.MOVEMENT_PLAYER, m_TouchStartPosition, touch);
+            else
+                GameManager.instance.EventManager.TriggerEvent(Constants.CAMERA_MOVEMENT, m_TouchStartPosition, touch);
+        }
+        else if(Input.touchCount == 2)
+        {
+
+        }
+
     }
 
-    public void GetStartEndTouches()
+    public void GetStartTouch(Touch touch)
     {
+        Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.y, m_Ball.transform.position.y, touch.position.x));
 
+        if (touch.phase == TouchPhase.Began)
+        {
+            m_TouchStartPosition = touchPosition;
+        }
     }
 
-    public bool CheckDeadzoneBall()
+    public bool CheckInDeadzoneBall()
     {
-        return true;
+        float distance = Vector3.Distance(m_TouchStartPosition, m_Ball.transform.position);
+
+        if (distance < m_DeadZoneBallRadius)
+            return true;
+        else
+            return false;
     }
 }
--- Assets/Scripts/Player/MovementComponent.cs
+++ Assets/Scripts/Player/MovementComponent.cs
@@ -6,11 +6,9 @@ public class MovementComponent : MonoBehaviour
 {
     private Rigidbody m_RigidBody;
 
-    // Start is called before the first frame update
-
     private void Start()
     {
-        GameManager.instance.EventManager.Register(Constants.MOVEMENT_PLAYER, Movement);
+        
     }
 
     public void Movement(object[] param)
