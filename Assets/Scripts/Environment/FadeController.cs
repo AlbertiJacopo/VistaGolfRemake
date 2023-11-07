2f2550fd228f4ef3d4dc1c0bf4906e3e7e35ab6f
Tasogare_no_Jack <jacopoalberti363@gmail.com>
2023-11-07 14:39:45 +0100

    Changed fading script

--- Assets/Scripts/Environment/FadeController.cs
+++ Assets/Scripts/Environment/FadeController.cs
@@ -1,52 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.iOS;
 using UnityEngine;
+using static UnityEngine.Rendering.Universal.UniversalRenderPipelineEditorResources;
 
 public class FadeController : MonoBehaviour
 {
-    [SerializeField] private float m_ReducingAlphaPercent;
-    [SerializeField] private Material m_TransparentMaterial;
+    //[SerializeField] private float m_ReducingAlphaPercent;
+    //[SerializeField] private Material m_TransparentMaterial;
+    [SerializeField] string m_FadedMaterialName;
+    [SerializeField] string m_NormalMaterialName;
     private Transform m_Ball;
-    private Material[] m_PreviousMaterials;
-    private GameObject[] m_PreviousGameObjects;
+    private Transform m_Camera;
+    //private Material[] m_PreviousMaterials;
+    //private GameObject[] m_PreviousGameObjects;
 
     private void Start()
     {
-        GameManager.instance.EventManager.Register(Constants.UPDATE_CAMERA_ROTATION, FadingMaterial);
+        GameObject ball = GameObject.FindGameObjectWithTag("Player");
+        GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        m_Ball = ball.transform;
+        m_Camera = mainCamera.transform;
+        GameManager.instance.EventManager.Register(Constants.UPDATE_CAMERA_ROTATION, ChangeObstacleMaterial);
     }
 
-    public void FadingMaterial(object[] param)
+    //public void FadingMaterial(object[] param)
+    //{
+    //    Vector3 dir = m_Ball.position - transform.position;
+    //    float distance = Vector3.Distance(m_Ball.position, transform.position);
+
+    //    RaycastHit[] hits = Physics.RaycastAll(transform.position, dir, distance);
+
+    //    m_PreviousMaterials = new Material[hits.Length - 1];
+    //    m_PreviousGameObjects = new GameObject[hits.Length - 1];
+
+    //    if (hits.Length > 1)
+    //    {
+    //        for (int i = 0; i < hits.Length - 1; i++)
+    //        {
+    //            if (hits[i].collider.gameObject.transform.position != m_Ball.position)
+    //            {
+    //                m_PreviousGameObjects[i] = hits[i].collider.gameObject;
+    //                m_PreviousMaterials[i] = hits[i].collider.gameObject.GetComponent<MeshRenderer>().material;
+    //                Color tmp = m_PreviousMaterials[i].color;
+    //                tmp.a = 1f - m_ReducingAlphaPercent;
+    //                m_TransparentMaterial.color = tmp;
+    //                hits[i].collider.gameObject.GetComponent<MeshRenderer>().material = m_TransparentMaterial;
+    //            }
+    //        }
+    //    }
+    //}
+
+    //private void OnValidate()
+    //{
+    //    if(m_Ball == null)
+    //    {
+    //        m_Ball = FindObjectOfType<MovementComponent>().transform;
+    //    }
+    //}
+
+    public void ChangeObstacleMaterial(object[] param)
     {
-        Vector3 dir = m_Ball.position - transform.position;
-        float distance = Vector3.Distance(m_Ball.position, transform.position);
-
-        RaycastHit[] hits = Physics.RaycastAll(transform.position, dir, distance);
-
-        m_PreviousMaterials = new Material[hits.Length - 1];
-        m_PreviousGameObjects = new GameObject[hits.Length - 1];
-
-        if (hits.Length > 1)
+        if (Vector3.Distance(m_Ball.position, transform.position) < Vector3.Distance(m_Ball.position, m_Camera.position)/2 && 
+            Vector3.Distance(m_Camera.position, transform.position) < Vector3.Distance(m_Ball.position, m_Camera.position) / 2)
         {
-            for (int i = 0; i < hits.Length - 1; i++)
-            {
-                if (hits[i].collider.gameObject.transform.position != m_Ball.position)
-                {
-                    m_PreviousGameObjects[i] = hits[i].collider.gameObject;
-                    m_PreviousMaterials[i] = hits[i].collider.gameObject.GetComponent<MeshRenderer>().material;
-                    Color tmp = m_PreviousMaterials[i].color;
-                    tmp.a = 1f - m_ReducingAlphaPercent;
-                    m_TransparentMaterial.color = tmp;
-                    hits[i].collider.gameObject.GetComponent<MeshRenderer>().material = m_TransparentMaterial;
-                }
-            }
+            Resources.Load<Material>("Material/" + m_FadedMaterialName);
         }
-    }
-
-    private void OnValidate()
-    {
-        if(m_Ball == null)
+        else
         {
-            m_Ball = FindObjectOfType<MovementComponent>().transform;
+            Resources.Load<Material>(m_NormalMaterialName);
         }
     }
 }
