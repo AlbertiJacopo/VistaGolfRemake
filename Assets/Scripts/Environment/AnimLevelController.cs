5ffd6e1ee1a2aef82e7422611d107ea546f3aae7
Arthley <arthleyfamorca@gmail.com>
2023-11-08 21:47:07 +0100

    Fixed Anim and add AudioMixer

---
+++ Assets/Scripts/Environment/AnimLevelController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimLevelController : MonoBehaviour
+{
+
+	[SerializeField] private Animator m_Level;
+	[SerializeField] private GameObject m_Ball;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		GameManager.instance.EventManager.Register(Constants.ANIM_START_SINK, PlaySinkAnimaton);
+		GameManager.instance.EventManager.Register(Constants.TOGGLE_BALL, BallToggle);
+		StartCoroutine(WaitTimeBallAppear());
+	}
+
+	public void PlaySinkAnimaton(object[] param)
+	{
+		m_Level.SetBool("EndLevel", true);
+
+		StartCoroutine(WaitForEndAnim());
+	}
+
+	private IEnumerator WaitForEndAnim()
+	{
+		yield return new WaitForSeconds(m_Level.GetCurrentAnimatorStateInfo(0).length);
+		GameManager.instance.EventManager.TriggerEvent(Constants.LOAD_NEXT_LEVEL);
+	}
+
+	private IEnumerator WaitTimeBallAppear()
+	{
+		yield return new WaitForSeconds(m_Level.GetCurrentAnimatorStateInfo(0).length);
+		BallToggle(null);
+	}
+
+	public void BallToggle(object[] param)
+	{
+		if (m_Ball.activeSelf)
+			m_Ball.SetActive(false);
+		else if (!m_Ball.activeSelf)
+			m_Ball.SetActive(true);
+	}
+}
